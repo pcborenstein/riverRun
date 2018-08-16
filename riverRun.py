80ed35f7c020d4d63306ae2276406b0d89fc161f
Peter <pcborenstein@gmail.com>
2018-08-16 17:16:46 -0400

    trying to get the HTML for the results

---
+++ riverRun.py
@@ -0,0 +1,19 @@
+import socket
+
+
+
+s = socket.socket()
+
+host=socket.gethostbyname('www.1stplacesports.com')
+port = 80
+s.connect((host, port))
+print("host: ", host)
+#s.sendall("GET /grr16overallres.htm\r\nHost: www.1stplacesports.com\r\n")
+s.sendall("""
+GET /grr16overallres.htm HTTP/1.1\r\n
+Host: www.1stplacesports.com\r\n
+\r\n
+""")
+val = s.recv(1000)
+
+print(val)
